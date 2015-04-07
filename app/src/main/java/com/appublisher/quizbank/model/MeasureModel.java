c1509ecee730f52e9f1cf45f0a977b8e079a97e4
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-04-07 22:41:45 +0800

    富文本控件添加

--- app/src/main/java/com/appublisher/quizbank/model/MeasureModel.java
+++ app/src/main/java/com/appublisher/quizbank/model/MeasureModel.java
@@ -1,7 +1,30 @@
 package com.appublisher.quizbank.model;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.appublisher.quizbank.R;
+import com.appublisher.quizbank.activity.ScaleImageActivity;
+import com.appublisher.quizbank.model.richtext.IParser;
+import com.appublisher.quizbank.model.richtext.ImageParser;
+import com.appublisher.quizbank.model.richtext.MatchInfo;
+import com.appublisher.quizbank.model.richtext.ParseManager;
+import com.appublisher.quizbank.network.Request;
+
+import org.apmem.tools.layouts.FlowLayout;
+
+import java.util.ArrayList;
 
 /**
  * 做题模块
@@ -43,5 +66,86 @@ public class MeasureModel {
         layoutParams.width = width;
         layoutParams.height = height;
         view.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * 动态添加富文本
+     * @param activity Activity
+     * @param container 富文本控件容器
+     * @param rich 富文本
+     */
+    public static void addRichTextToContainer(final Activity activity, LinearLayout container, String rich) {
+        Request request = new Request(activity);
+
+        // 通过迭代装饰方式构造解析器
+        IParser parser = new ImageParser(activity);
+
+        // 执行解析并返回解析文本段队列
+        ParseManager manager = new ParseManager();
+        ArrayList<ParseManager.ParsedSegment> segments = manager.parse(parser, rich);
+
+        // 用 Holder 模式更新列表数据
+        FlowLayout flowLayout = new FlowLayout(activity);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(
+                AbsListView.LayoutParams.MATCH_PARENT,
+                AbsListView.LayoutParams.WRAP_CONTENT);
+        flowLayout.setLayoutParams(params);
+        flowLayout.setGravity(Gravity.CENTER_VERTICAL);
+
+        for (final ParseManager.ParsedSegment segment : segments) {
+            if (TextUtils.isEmpty(segment.text)) {
+                continue;
+            }
+
+            if (MatchInfo.MatchType.None == segment.type) {
+                TextView textView = new TextView(activity);
+                LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+                textView.setLayoutParams(p);
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 17);
+                textView.setTextColor(activity.getResources().getColor(R.color.setting_text));
+                textView.setClickable(false);
+                flowLayout.addView(textView);
+                textView.setText(segment.text);
+
+            } else if (MatchInfo.MatchType.Image == segment.type) {
+                ImageView imgView = new ImageView(activity);
+                LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+                imgView.setLayoutParams(p);
+
+                flowLayout.addView(imgView);
+
+                // 异步加载图片
+                request.loadImage(segment.text.toString(), imgView);
+
+                imgView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent();
+                        intent.setClass(activity, ScaleImageActivity.class);
+                        intent.putExtra("imgUrl", segment.text.toString());
+                        activity.startActivity(intent);
+                    }
+                });
+
+                // 如果图片太小，扩大两倍
+                int imgWidth = MeasureModel.getViewWidth(imgView);
+                int imgHeight = MeasureModel.getViewHeight(imgView);
+
+                DisplayMetrics dm = activity.getResources().getDisplayMetrics();
+                int screenHeight = dm.heightPixels - 50; // 50是状态栏高度
+
+                if (imgHeight < screenHeight / 10) {
+                    imgWidth = imgWidth * 2;
+                    imgHeight = imgHeight * 2;
+                    MeasureModel.setViewWidthAndHeight(imgView, imgWidth, imgHeight);
+                }
+            }
+        }
+
+        container.addView(flowLayout);
     }
 }
