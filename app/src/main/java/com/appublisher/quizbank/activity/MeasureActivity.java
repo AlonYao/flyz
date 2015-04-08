0ced334d397a1ee79ce488dfe1ddc66d87aeb76f
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-04-08 11:10:07 +0800

    带材料题目的图文混排实现

--- app/src/main/java/com/appublisher/quizbank/activity/MeasureActivity.java
+++ app/src/main/java/com/appublisher/quizbank/activity/MeasureActivity.java
@@ -1,37 +1,22 @@
 package com.appublisher.quizbank.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.util.DisplayMetrics;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
 
 import com.appublisher.quizbank.R;
 import com.appublisher.quizbank.model.CommonModel;
 import com.appublisher.quizbank.model.MeasureModel;
-import com.appublisher.quizbank.model.richtext.IParser;
-import com.appublisher.quizbank.model.richtext.ImageParser;
-import com.appublisher.quizbank.model.richtext.MatchInfo;
-import com.appublisher.quizbank.model.richtext.ParseManager;
 import com.appublisher.quizbank.network.Request;
 import com.appublisher.quizbank.utils.Logger;
-
-import org.apmem.tools.layouts.FlowLayout;
-
-import java.util.ArrayList;
 
 /**
  * 做题
@@ -52,10 +37,20 @@ public class MeasureActivity extends ActionBarActivity {
         CommonModel.setToolBar(this);
 
         // View 初始化
-        ImageView iv = (ImageView) findViewById(R.id.measuer_iv);
+        ImageView iv = (ImageView) findViewById(R.id.measure_iv);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        final ScrollView svTop = (ScrollView) findViewById(R.id.measuer_top);
-        LinearLayout llQuestionContent = (LinearLayout) findViewById(R.id.measuer_question_content);
+        final ScrollView svTop = (ScrollView) findViewById(R.id.measure_top);
+        LinearLayout llQuestionContent = (LinearLayout) findViewById(R.id.measure_question_content);
+        LinearLayout llOptionAContainer = (LinearLayout) findViewById(R.id.measure_option_a_container);
+        LinearLayout llOptionBContainer = (LinearLayout) findViewById(R.id.measure_option_b_container);
+        LinearLayout llOptionCContainer = (LinearLayout) findViewById(R.id.measure_option_c_container);
+        LinearLayout llOptionDContainer = (LinearLayout) findViewById(R.id.measure_option_d_container);
+        LinearLayout llQuestionMaterial = (LinearLayout) findViewById(R.id.measure_material);
+        LinearLayout llQuestionContentM = (LinearLayout) findViewById(R.id.measure_question_content_m);
+        LinearLayout llOptionAContainerM = (LinearLayout) findViewById(R.id.measure_option_a_container_m);
+        LinearLayout llOptionBContainerM = (LinearLayout) findViewById(R.id.measure_option_b_container_m);
+        LinearLayout llOptionCContainerM = (LinearLayout) findViewById(R.id.measure_option_c_container_m);
+        LinearLayout llOptionDContainerM = (LinearLayout) findViewById(R.id.measure_option_d_container_m);
 
         Request request = new Request(this);
 
@@ -64,9 +59,24 @@ public class MeasureActivity extends ActionBarActivity {
         screenWidth = dm.widthPixels;
         screenHeight = dm.heightPixels - 50; // 50是状态栏高度
 
-        // 题干
         String rich = "把1月和2月的利润代入公式，我们可以得到<img=http://dl.cdn.appublisher.com/yimgs/4/gjkodixmzjizdmz.png></img> ，解得<img=http://dl.cdn.appublisher.com/yimgs/4/wq3mjhjywjhyzzj.png></img>。故1—12月的累积利润为<img=http://dl.cdn.appublisher.com/yimgs/4/dg1y2e3mgm0mdq0.png></img> ，平均利润为<img=http://dl.cdn.appublisher.com/yimgs/4/2m4mzg3zjjiownk.png></img>。因此，本题答案选择C选项。";
+        /** 带材料的题目 */
+        MeasureModel.addRichTextToContainer(this, llQuestionMaterial, rich);
+        MeasureModel.addRichTextToContainer(this, llQuestionContentM, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionAContainerM, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionBContainerM, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionCContainerM, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionDContainerM, rich);
+
+        /** 不带材料的题目 */
+        // 题干
         MeasureModel.addRichTextToContainer(this, llQuestionContent, rich);
+
+        // 选项
+        MeasureModel.addRichTextToContainer(this, llOptionAContainer, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionBContainer, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionCContainer, rich);
+        MeasureModel.addRichTextToContainer(this, llOptionDContainer, rich);
 
         int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
         int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
