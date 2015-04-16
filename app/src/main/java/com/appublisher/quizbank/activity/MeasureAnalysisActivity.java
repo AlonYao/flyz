3492578a2bdb62dcad4f67e3b2ef094086f704b1
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-04-16 18:02:38 +0800

    去掉无用的Activity

--- app/src/main/java/com/appublisher/quizbank/activity/MeasureAnalysisActivity.java
+++ app/src/main/java/com/appublisher/quizbank/activity/MeasureAnalysisActivity.java
@@ -1,25 +1,25 @@
 package com.appublisher.quizbank.activity;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.appublisher.quizbank.R;
 
-public class SpecialPracticeActivity extends ActionBarActivity {
+public class MeasureAnalysisActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_special_practice);
+        setContentView(R.layout.activity_measure_analysis);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_special_practice, menu);
+        getMenuInflater().inflate(R.menu.menu_measure_analysis, menu);
         return true;
     }
 
