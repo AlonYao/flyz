78922955323b8944eac4d2ced8af8bb1c3b5c773
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-04-22 12:30:14 +0800

    整卷练习Menu 子菜单UI

--- app/src/main/java/com/appublisher/quizbank/fragment/WholePageFragment.java
+++ app/src/main/java/com/appublisher/quizbank/fragment/WholePageFragment.java
@@ -1,5 +1,6 @@
 package com.appublisher.quizbank.fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import com.appublisher.quizbank.R;
 
@@ -16,6 +19,7 @@ import com.appublisher.quizbank.R;
 public class WholePageFragment extends Fragment{
 
     private Activity mActivity;
+    private PopupWindow mPwProvince;
 
     @Override
     public void onAttach(Activity activity) {
@@ -31,7 +35,36 @@ public class WholePageFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        // view 初始化
         View view = inflater.inflate(R.layout.fragment_wholepage, container, false);
+        RelativeLayout rlProvince = (RelativeLayout) view.findViewById(R.id.wholepage_province_rl);
+
+        // 省份
+        rlProvince.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return view;
+    }
+
+    /**
+     * 初始化省份菜单
+     */
+    private void initPwProvince() {
+        @SuppressLint("InflateParams") View view =
+                LayoutInflater.from(mActivity).inflate(R.layout.wholepage_popup_province, null);
+
+        mPwProvince = new PopupWindow(
+                view,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+        mPwProvince.setOutsideTouchable(true);
+        mPwProvince.setBackgroundDrawable(
+                mActivity.getResources().getDrawable(R.color.transparency));
+        mPwProvince.update();
     }
 }
