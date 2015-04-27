7dd758efd3792860382d386c39523db96fc7445a
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-04-27 12:09:54 +0800

    学习记录页面UI初步

--- app/src/main/java/com/appublisher/quizbank/fragment/StudyRecordFragment.java
+++ app/src/main/java/com/appublisher/quizbank/fragment/StudyRecordFragment.java
@@ -1,5 +1,6 @@
 package com.appublisher.quizbank.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,10 +15,21 @@ import com.appublisher.quizbank.R;
  */
 public class StudyRecordFragment extends Fragment{
 
+    private Activity mActivity;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        // View 初始化
         View view = inflater.inflate(R.layout.fragment_studyrecord, container, false);
+
+
         return view;
     }
 }
