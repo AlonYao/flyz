f65a47254f9efa5c03080e2cc9831ac42d65ce5d
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-05-20 15:01:59 +0800

    新浪微博回调地址修改

--- app/src/main/java/com/appublisher/quizbank/model/login/activity/LoginActivity.java
+++ app/src/main/java/com/appublisher/quizbank/model/login/activity/LoginActivity.java
@@ -189,7 +189,7 @@ public class LoginActivity extends ActionBarActivity implements RequestCallback{
 
         // 微博 设置新浪SSO handler
         mController.getConfig().setSsoHandler(new SinaSsoHandler());
-        mController.getConfig().setSinaCallbackUrl("http://www.sina.com");
+        mController.getConfig().setSinaCallbackUrl("http://sns.whalecloud.com/sina2/callback");
         weiboBtn.setOnClickListener(mLoginModel.weiboOnClick);
 
         // 保存Activity
