e2c889853ab91f6762fd2b5eb7f3d432933ee6ca
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-07-14 16:16:43 +0800

    手机号注册模块初步

--- app/src/main/java/com/appublisher/quizbank/model/login/activity/MobileRegisterActivity.java
+++ app/src/main/java/com/appublisher/quizbank/model/login/activity/MobileRegisterActivity.java
@@ -1,37 +1,50 @@
 package com.appublisher.quizbank.model.login.activity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.appublisher.quizbank.QuizBankApp;
 import com.appublisher.quizbank.R;
+import com.appublisher.quizbank.model.business.CommonModel;
 
+/**
+ * 手机号注册
+ */
 public class MobileRegisterActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_register);
+
+        // ActionBar
+        CommonModel.setToolBar(this);
+
+        // View
+
+        // 保存Activity
+        QuizBankApp.getInstance().addActivity(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mobile_register, menu);
-        return true;
+        menu.clear();
+        MenuItemCompat.setShowAsAction(menu.add("登录"), MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else if (item.getTitle().equals("登录")) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
