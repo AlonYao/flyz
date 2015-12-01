5e11ba29be7b40f7e1f2401049f132244accb99a
sunhuaxiao <sunhuaxiao@appublisher.com>
2015-12-01 11:03:54 +0800

    1）增加数据库升级的脚本文件  2）版本提升至1.4.1

---
+++ app/src/main/assets/migrations/5.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Offline ADD COLUMN course_id INTEGER;
--- app/src/main/java/com/appublisher/quizbank/common/offline/model/db/Offline.java
+++ app/src/main/java/com/appublisher/quizbank/common/offline/model/db/Offline.java
@@ -16,6 +16,8 @@ public class Offline extends Model {
 
     @Column(name = "is_success")
     public int is_success;
+
     @Column(name = "course_id")
     public int course_id;
+
 }
