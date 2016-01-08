e2e55fc2ec4f86c20745a788b4dbcf9cd20ce8d5
sunhuaxiao <sunhuaxiao@appublisher.com>
2016-01-08 11:51:51 +0800

    公开课模块：评价页面UI初步

---
+++ app/src/main/java/com/appublisher/quizbank/common/opencourse/activity/OpenCourseGradeActivity.java
@@ -0,0 +1,18 @@
+package com.appublisher.quizbank.common.opencourse.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appublisher.quizbank.R;
+
+/**
+ * 公开课评价页面
+ */
+public class OpenCourseGradeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_course_grade);
+    }
+}
