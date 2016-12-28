40dd499f1cdc098c84b3f769840215d802ab3b5c
sunhuaxiao <sunhuaxiao@appublisher.com>
2016-12-28 22:49:07 +0800

    增加toolbar search

---
+++ app/src/main/java/com/appublisher/quizbank/common/measure/activity/MeasureSearchActivity.java
@@ -0,0 +1,16 @@
+package com.appublisher.quizbank.common.measure.activity;
+
+import android.os.Bundle;
+
+import com.appublisher.lib_basic.activity.BaseActivity;
+import com.appublisher.quizbank.R;
+
+public class MeasureSearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measure_search);
+        setToolBar(this);
+    }
+}
