6f33647a2d135a0a3bdbc1809ab62e5b143ea1a7
sunhuaxiao <sunhuaxiao@appublisher.com>
2016-12-30 18:50:29 +0800

    做题搜索逻辑实现

--- app/src/main/java/com/appublisher/quizbank/common/measure/activity/MeasureSearchActivity.java
+++ app/src/main/java/com/appublisher/quizbank/common/measure/activity/MeasureSearchActivity.java
@@ -1,11 +1,16 @@
 package com.appublisher.quizbank.common.measure.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.appublisher.lib_basic.activity.BaseActivity;
 import com.appublisher.quizbank.R;
 
-public class MeasureSearchActivity extends BaseActivity {
+public class MeasureSearchActivity extends BaseActivity implements View.OnClickListener{
+
+    private EditText mEtSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,23 @@ public class MeasureSearchActivity extends BaseActivity {
     }
 
     private void initView() {
+        ImageButton btnSearch = (ImageButton) findViewById(R.id.measure_search_btn);
+        if (btnSearch != null) {
+            btnSearch.setVisibility(View.VISIBLE);
+            btnSearch.setOnClickListener(this);
+        }
 
+        mEtSearch = (EditText) findViewById(R.id.measure_search_et);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.measure_search_btn:
+                if (mEtSearch == null) return;
+                String text = mEtSearch.getText().toString();
+                if (text.length() == 0) return;
+                break;
+        }
     }
 }
